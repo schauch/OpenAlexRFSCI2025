f02b04babafe34dc9dce6c7fede92db874632c0f
Teresa <teresas@unr.edu>
2025-07-16 15:26:08 -0700

    Update RCode_Practice.R

--- RCode_Practice.R
+++ RCode_Practice.R
@@ -1,15 +1,1 @@
-2+4
-
-six <- 2+4
-seven <- six + 1
-seven
-
-
-animal <- c('koala', 'hedgehog', 'sloth', 'panda') 
-country <- c('Australia', 'Italy', 'Peru', 'China')
-avg_sleep_hours <- c(21, 18, 17, 10)
-
-super_sleepers <- data.frame(animal, country, avg_sleep_hours)
-
-animal
-getwd()
+print("hello world")

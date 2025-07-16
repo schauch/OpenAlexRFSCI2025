a5752db84f9cf168cc689c4a88b3df444f0bbba4
Teresa S <tmauch@gmail.com>
2025-07-16 15:22:13 -0700

    Removed content from practice code

--- RCode_Practice.R
+++ RCode_Practice.R
@@ -4,7 +4,12 @@ six <- 2+4
 seven <- six + 1
 seven
 
+#name <- command
+text <- "Hello world"
+text
 
+#Here is how you use this code
+#More instructions here
 animal <- c('koala', 'hedgehog', 'sloth', 'panda') 
 country <- c('Australia', 'Italy', 'Peru', 'China')
 avg_sleep_hours <- c(21, 18, 17, 10)
@@ -13,3 +18,4 @@ super_sleepers <- data.frame(animal, country, avg_sleep_hours)
 
 animal
 getwd()
+
